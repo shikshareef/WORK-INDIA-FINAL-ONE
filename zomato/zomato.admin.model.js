57fa2126471ff926874416d87f562cfc8ee7dd3f
shikshareef <106966273+shikshareef@users.noreply.github.com>
2024-08-06 10:46:42 +0530

    Update zomato.admin.model.js

--- zomato/zomato.admin.model.js
+++ zomato/zomato.admin.model.js
@@ -1,19 +1,17 @@
-
 var dbConn = require('./db.config');
 
-// User object create
+// Venue object create
 const adminKey = "WorkIndia";
 
-
-var Venue = function(place){
+var Venue = function(place) {
     this.name = place.name;
     this.address = place.address;
     this.phone_no = place.phone_no;
     this.website = place.website;
 }
 
-Venue.create = function(newUser, result) {
-    dbConn.query("INSERT INTO Venue set ?", newVenue, function(err, res) {
+Venue.create = function(newVenue, result) {
+    dbConn.query("INSERT INTO venue SET ?", newVenue, function(err, res) {
         if (err) {
             console.log("error: ", err);
             result(err, null);
@@ -24,24 +22,5 @@ Venue.create = function(newUser, result) {
     });
 };
 
-
-
-
-User.login = function(email, password, result) {
-    dbConn.query("SELECT * FROM USER WHERE email = ? AND pass = ?", [email, password], function(err, rows) {
-        if (err) {
-            console.error(err);
-            return result(err, null);
-        }
-        if (rows.length > 0) {
-            return result(null, rows[0]);
-        } else {
-            return result({ message: "Invalid email or password" }, null);
-        }
-    });
-};
-
-
-
-
-module.exports = User;
+// Export the Venue object
+module.exports = Venue;
